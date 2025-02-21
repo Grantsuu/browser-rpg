d7a11b3aa6093f14148a3da4f267386e1077eb06
Grant Sherman <tnarg122@gmail.com>
2025-02-21 07:20:42 +0000

    installed daisyui and react router started home page and removed app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
-import App from './App.tsx'
+import Home from './pages/Home.tsx'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Home />} />
+                {/* <Route element={<AuthLayout />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                </Route> */}
+            </Routes>
+        </BrowserRouter>
     </StrictMode>,
 )
