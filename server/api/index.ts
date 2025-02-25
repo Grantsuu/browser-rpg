9d4c4a27b77ff8fb2f39cc2106b5f1d31ce896c0
Grant Sherman <tnarg122@gmail.com>
2025-02-25 23:10:14 +0000

    use hono create

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,16 +1,14 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
+export const config = {
+  runtime: 'edge'
+}
+
 const app = new Hono().basePath('/api')
 
 app.get('/', (c) => {
-    return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
+  return c.json({ message: 'Hello Hono!' })
 })
 
-const handler = handle(app);
-
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default handle(app)
