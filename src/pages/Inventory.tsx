2ea96c840f1ae0d251a6f0f48be5bb2f9f50975c
Grant Sherman <tnarg122@gmail.com>
2025-02-27 07:16:08 +0000

    pull actual inventory data from supabase

--- src/pages/Inventory.tsx
+++ src/pages/Inventory.tsx
@@ -7,7 +7,7 @@ import { useSupabase } from "../contexts/SupabaseContext"
 interface item {
     image: string,
     name: string,
-    type: string,
+    category: string,
     amount: number,
     value: number,
     description: string
@@ -20,71 +20,111 @@ const Inventory = () => {
     const [inventory, setInventory] = useState<item[]>([]);
 
     const handleGetInventory = async () => {
+        setLoading(true);
+        // Get character UUID
         if (supabaseClient && supabaseUser) {
             const character = await supabaseClient
                 .from('characters')
                 .select('id')
                 .eq('user', supabaseUser.id);
-            // console.log(character);
+            // Get all inventory for character
             if (character.data && character.data.length > 0) {
-                const inventory = await supabaseClient
+                const characterInventory = await supabaseClient
                     .from('inventories')
                     .select()
                     .eq('character', character.data[0].id);
-                console.log(inventory);
+                // Get items in inventory
+                if (characterInventory.data && characterInventory.data.length > 0) {
+                    characterInventory.data.map(async (item) => {
+                        // console.log(item);
+                        const newItem: item = {
+                            image: '',
+                            name: '',
+                            category: '',
+                            amount: item.amount,
+                            value: 0,
+                            description: ''
+                        };
+                        // Get item
+                        const inventoryItem = await supabaseClient
+                            .from('items')
+                            .select()
+                            .eq('id', item.item);
+                        if (inventoryItem.data && inventoryItem.data.length > 0) {
+                            newItem.name = inventoryItem.data[0].name;
+                            newItem.value = inventoryItem.data[0].value;
+                            newItem.description = inventoryItem.data[0].description;
+                            // Get item categories
+                            const itemCategory = await supabaseClient
+                                .from('item_categories')
+                                .select('name')
+                                .eq('id', inventoryItem.data[0].category);
+                            if (itemCategory.data && itemCategory.data.length > 0) {
+                                newItem.category = itemCategory.data[0].name;
+                            }
+                            // Get item images
+                            const itemImage = await supabaseClient
+                                .from('item_images')
+                                .select('base64')
+                                .eq('id', inventoryItem.data[0].image);
+                            if (itemImage.data && itemImage.data.length > 0) {
+                                newItem.image = itemImage.data[0].base64;
+                            }
+                        }
+                        setInventory([...inventory, newItem]);
+                    })
+                }
             }
         }
+        setLoading(false);
     }
 
-    handleGetInventory();
-
-    const items: item[] = [
-        {
-            image: "items/blazing_edge.svg",
-            name: "Blazing Edge",
-            type: "Weapon",
-            amount: 1,
-            value: 2500,
-            description: "A sword imbued with the essence of fire, burning enemies with each strike."
-        },
-        {
-            image: "items/ring_of_arcane.svg",
-            name: "Ring of Arcane Insight",
-            type: "Accessory",
-            amount: 1,
-            value: 1800,
-            description: "An enchanted ring that enhances the wearer’s magical abilities and intellect."
-        },
-        {
-            image: "items/potion_of_rejuvination.svg",
-            name: "Potion of Rejuvenation",
-            type: "Consumable",
-            amount: 3,
-            value: 300,
-            description: "A rare potion that instantly heals wounds and restores stamina."
-        },
-        {
-            image: "items/cloak_phantom.svg",
-            name: "Cloak of the Phantom",
-            type: "Armor",
-            amount: 1,
-            value: 2200,
-            description: "A dark, ethereal cloak that grants invisibility for a short period when activated."
-        },
-        {
-            image: "items/dragon_scale.svg",
-            name: "Dragon Scale",
-            type: "Ingredient",
-            amount: 3,
-            value: 2000,
-            description: "A shimmering scale from an ancient dragon, used in crafting indestructible armor and potions of fortitude."
-        }
-    ];
+    // const items: item[] = [
+    //     {
+    //         image: "items/blazing_edge.svg",
+    //         name: "Blazing Edge",
+    //         category: "Weapon",
+    //         amount: 1,
+    //         value: 2500,
+    //         description: "A sword imbued with the essence of fire, burning enemies with each strike."
+    //     },
+    //     {
+    //         image: "items/ring_of_arcane.svg",
+    //         name: "Ring of Arcane Insight",
+    //         category: "Accessory",
+    //         amount: 1,
+    //         value: 1800,
+    //         description: "An enchanted ring that enhances the wearer’s magical abilities and intellect."
+    //     },
+    //     {
+    //         image: "items/potion_of_rejuvination.svg",
+    //         name: "Potion of Rejuvenation",
+    //         category: "Consumable",
+    //         amount: 3,
+    //         value: 300,
+    //         description: "A rare potion that instantly heals wounds and restores stamina."
+    //     },
+    //     {
+    //         image: "items/cloak_phantom.svg",
+    //         name: "Cloak of the Phantom",
+    //         category: "Armor",
+    //         amount: 1,
+    //         value: 2200,
+    //         description: "A dark, ethereal cloak that grants invisibility for a short period when activated."
+    //     },
+    //     {
+    //         image: "items/dragon_scale.svg",
+    //         name: "Dragon Scale",
+    //         category: "Ingredient",
+    //         amount: 3,
+    //         value: 2000,
+    //         description: "A shimmering scale from an ancient dragon, used in crafting indestructible armor and potions of fortitude."
+    //     }
+    // ];
 
     useEffect(() => {
-        // setLoading(true);
-        setInventory(items);
-        setLoading(false);
+        handleGetInventory();
+        // eslint-disable-next-line
     }, []);
 
     return (
@@ -113,18 +153,18 @@ const Inventory = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {inventory.map((item: item) => {
+                                    {inventory.map((item: item, id) => {
                                         return (
-                                            <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0">
-                                                <td className="m-0">
-                                                    <img src={item.image} className="w-10" />
+                                            <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0" key={id}>
+                                                <td className="m-0 w-1/16">
+                                                    <img src={`data:image/svg+xml;base64,${item.image}`} />
                                                 </td>
                                                 <td>
                                                     {item.name}
                                                 </td>
                                                 <td>
                                                     <span className="badge badge-soft badge-primary badge-sm">
-                                                        {item.type}
+                                                        {item.category}
                                                     </span>
                                                 </td>
                                                 <td>
