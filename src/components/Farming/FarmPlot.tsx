0921732ebdb15aa1eb551bcb10cd8958955d7264
Grant Sherman <tnarg122@gmail.com>
2025-03-12 22:43:09 +0000

    starting farm plot design mockups

---
+++ src/components/Farming/FarmPlot.tsx
@@ -0,0 +1,31 @@
+const FarmPlot = () => {
+    return (
+        <div className="card w-full bg-base-100 card-lg shadow-sm">
+            <div className="card-body">
+                <div className="flex w-full items-center justify-center">
+                    <h2 className="card-title text-center">Farm Plot</h2>
+                </div>
+                <div className="flex justify-center">
+                    <img src="/shield-halved-solid.svg" className="w-25" />
+                </div>
+                <div className="prose">
+                    <div><b>Status:</b> Inactive, Growing, Ready to Harvest</div>
+                    <div><b>Contents:</b> Wheat Seeds</div>
+
+                    <div className="flex flew-row items-baseline gap-2">
+                        <div><b>Progress:</b></div>
+                        <progress className="progress progress-success w-full" value="100" max="100"></progress>
+                        30s
+                    </div>
+                </div>
+                <div className="justify-center card-actions">
+                    <button className="btn btn-primary btn-wide">Plant Seeds</button>
+                    <button className="btn btn-error btn-wide">Cancel</button>
+                    <button className="btn btn-secondary btn-wide">Harvest</button>
+                </div>
+            </div>
+        </div >
+    );
+};
+
+export default FarmPlot;
--- src/containers/Farming.tsx
+++ src/containers/Farming.tsx
@@ -1,17 +1,31 @@
 import { useState, useEffect } from 'react';
 import { faSeedling } from "@fortawesome/free-solid-svg-icons";
 import PageCard from '../layouts/PageCard';
+import FarmPlot from '../components/Farming/FarmPlot';
+
+// const MAX_PLOTS = 3;
 
 const Farming = () => {
     const [loading, setLoading] = useState(false);
+
+    const [plots, setPlots] = useState([0, 0, 0]);
+
     useEffect(() => {
         console.log(loading);
         setLoading(false);
     }, []);
 
     return (
-        <PageCard title="Farming" icon={faSeedling} >
-            <></>
+        <PageCard title="Farming" icon={faSeedling}>
+            <div className="grid grid-cols-3 gap-4">
+                {
+                    plots.map((index) => {
+                        return (
+                            <FarmPlot key={index} />
+                        )
+                    })
+                }
+            </div>
         </PageCard>
     )
 }
