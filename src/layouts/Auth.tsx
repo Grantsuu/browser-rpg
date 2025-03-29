b76a75c89356d1ddd378f8fbd544ac467019e9d8
Grant Sherman <tnarg122@gmail.com>
2025-03-29 02:41:39 +0000

    dvh instead of screen for mobile

--- src/layouts/Auth.tsx
+++ src/layouts/Auth.tsx
@@ -5,14 +5,14 @@ import { IconProp } from "@fortawesome/fontawesome-svg-core";
 
 const Auth = () => {
     return (
-        <div className="flex items-center justify-center w-full min-h-screen">
+        <div className="flex items-center justify-center w-full h-dvh">
             <div className="flex flex-col gap-4 w-full items-center justify-center">
                 <div className="prose">
                     <h1>
                         Browser RPG <FontAwesomeIcon icon={faShieldHalved as IconProp} />
                     </h1>
                 </div>
-                <div className="w-full md:w-1/2 lg:w-1/3">
+                <div className="w-full md:w-1/2 lg:w-1/4 xl:w-1/5">
                     <div className="card rounded-box shadow-xl bg-base-200">
                         <div className="card-body">
                             <Outlet />
