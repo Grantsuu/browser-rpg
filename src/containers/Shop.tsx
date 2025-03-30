58c53c99fb7f215b7d60e6cbef6abfef602d077e
Grant Sherman <tnarg122@gmail.com>
2025-03-30 04:14:21 +0000

    shop responsive

--- src/containers/Shop.tsx
+++ src/containers/Shop.tsx
@@ -96,8 +96,8 @@ const Shop = () => {
         <PageCard title="Shop" icon={faShop}>
             <div className="flex flex-row items-center justify-between">
                 <div className="join">
-                    <input className="join-item btn" type="radio" name="options" aria-label="Buy" defaultChecked onClick={() => setShopMode('buy')} />
-                    <input className="join-item btn" type="radio" name="options" aria-label="Sell" onClick={() => setShopMode('sell')} />
+                    <input className="join-item btn btn-wide" type="radio" name="options" aria-label="Buy" defaultChecked onClick={() => setShopMode('buy')} />
+                    <input className="join-item btn btn-wide" type="radio" name="options" aria-label="Sell" onClick={() => setShopMode('sell')} />
                 </div>
                 <div className="prose prose-lg">
                     <FontAwesomeIcon icon={faCoins as IconProp} className="mr-1" />
@@ -113,99 +113,97 @@ const Shop = () => {
                         )
                     })}
             </div>
-            <div className="flex flex-col overflow-y-scroll w-full h-full rounded border border-base-content/8 ">
-                <table className={`table table-pin-rows bg-base-100 ${(isInventoryLoading || isShopLoading) ? 'flex-1' : ''}`}>
-                    {/* head */}
-                    <thead>
-                        <tr className="bg-secondary">
-                            <th></th>
-                            <th>Name</th>
-                            <th>Category</th>
-                            {shopMode === 'sell' && <th>Amount</th>}
-                            <th>Value</th>
-                            <th>Description</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {(isInventoryLoading || isShopLoading) ?
-                            <tr>
-                                <td colSpan={7}>
-                                    <div className="flex items-center justify-center">
-                                        <span className="loading loading-spinner loading-xl"></span>
-                                    </div>
-                                </td>
-                            </tr> :
-                            shopMode === 'buy' ?
-                                shop.map((item: item, id: number) => {
-                                    return (
-                                        <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0" key={id}>
-                                            <td className="m-0 w-1/16">
-                                                <img src={item.image.base64} />
-                                            </td>
-                                            <td>
-                                                {item.name}
-                                            </td>
-                                            <td>
-                                                <ItemCategoryBadge category={item.category} />
-                                            </td>
-                                            <td>
-                                                {item.value}
-                                            </td>
-                                            <td>
-                                                {item.description}
-                                            </td>
-                                            <td>
-                                                <div className="flex flex-row gap-2">
-                                                    <button className="btn btn-soft btn-primary" onClick={() => { buy({ item: item, amount: 1 }) }} disabled={isBuyPending}>
-                                                        {isBuyPending ? <span className="loading loading-spinner loading-sm"></span> : 'Buy'}
-                                                    </button>
-                                                    <button className="btn btn-soft btn-primary" onClick={() => { buy({ item: item, amount: 5 }) }} disabled={isBuyPending}>
-                                                        {isBuyPending ? <span className="loading loading-spinner loading-sm"></span> : 'Buy x5'}
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    )
-                                }) :
-                                inventory.map((item: item, id: number) => {
-                                    return (
-                                        <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0" key={id}>
-                                            <td className="m-0 w-1/16">
-                                                <img src={item.image.base64} />
-                                            </td>
-                                            <td>
-                                                {item.name}
-                                            </td>
-                                            <td>
-                                                <ItemCategoryBadge category={item.category} />
-                                            </td>
-                                            <td>
-                                                {item.amount}
-                                            </td>
-                                            <td>
-                                                {item.value / 2}
-                                            </td>
-                                            <td>
-                                                {item.description}
-                                            </td>
-                                            <td>
-                                                <div className="flex flex-row gap-2">
-                                                    <button className="btn btn-soft btn-error" onClick={() => { sell({ item: item, amount: 5 }) }} disabled={isSellPending}>
-                                                        {isSellPending ? <span className="loading loading-spinner loading-sm"></span> : 'Sell'}
-                                                    </button>
-                                                    <button className="btn btn-soft btn-error" onClick={() => { sell({ item: item, amount: 5 }) }} disabled={isSellPending}>
-                                                        {isSellPending ? <span className="loading loading-spinner loading-sm"></span> : 'Sell x5'}
-                                                    </button>
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    )
-                                })
-                        }
-                    </tbody>
-                </table>
-            </div>
+            <table className={`xs:table-xs sm:table-sm md:table-md table-compact table-pin-rows bg-base-100 ${(isInventoryLoading || isShopLoading) ? 'flex-1' : ''}`}>
+                {/* head */}
+                <thead>
+                    <tr className="bg-secondary">
+                        <th className="w-10"></th>
+                        <th className="text-left p-1">Name</th>
+                        <th className="hidden xs:table-cell text-left p-1">Category</th>
+                        {shopMode === 'sell' && <th className="text-left p-1">Amount</th>}
+                        <th className="text-left p-1">Value</th>
+                        <th className="hidden xl:table-cell">Description</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {(isInventoryLoading || isShopLoading) ?
+                        <tr>
+                            <td colSpan={7}>
+                                <div className="flex items-center justify-center">
+                                    <span className="loading loading-spinner loading-xl"></span>
+                                </div>
+                            </td>
+                        </tr> :
+                        shopMode === 'buy' ?
+                            shop.map((item: item, id: number) => {
+                                return (
+                                    <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0" key={id}>
+                                        <td className="p-2 xs:p-1 w-15 xl:w-20">
+                                            <img src={item.image.base64} />
+                                        </td>
+                                        <td className="p-1">
+                                            {item.name}
+                                        </td>
+                                        <td className="hidden xs:table-cell p-1">
+                                            <ItemCategoryBadge category={item.category} />
+                                        </td>
+                                        <td className="p-1">
+                                            {item.value}
+                                        </td>
+                                        <td className="hidden xl:table-cell">
+                                            {item.description}
+                                        </td>
+                                        <td>
+                                            <div className="flex flex-row gap-1">
+                                                <button className="btn btn-soft btn-primary" onClick={() => { buy({ item: item, amount: 1 }) }} disabled={isBuyPending}>
+                                                    {isBuyPending ? <span className="loading loading-spinner loading-sm"></span> : 'Buy'}
+                                                </button>
+                                                <button className="btn btn-soft btn-primary" onClick={() => { buy({ item: item, amount: 5 }) }} disabled={isBuyPending}>
+                                                    {isBuyPending ? <span className="loading loading-spinner loading-sm"></span> : 'Buy x5'}
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            }) :
+                            inventory.map((item: item, id: number) => {
+                                return (
+                                    <tr className="table-row items-baseline justify-baseline hover:bg-base-300 m-0" key={id}>
+                                        <td className="p-2 xs:p-1 w-15 xl:w-20">
+                                            <img src={item.image.base64} />
+                                        </td>
+                                        <td>
+                                            {item.name}
+                                        </td>
+                                        <td className="hidden xs:table-cell p-1">
+                                            <ItemCategoryBadge category={item.category} />
+                                        </td>
+                                        <td>
+                                            {item.amount}
+                                        </td>
+                                        <td>
+                                            {Math.floor(item.value / 2)}
+                                        </td>
+                                        <td className="hidden xl:table-cell">
+                                            {item.description}
+                                        </td>
+                                        <td>
+                                            <div className="flex flex-col xs:flex-row gap-1">
+                                                <button className="btn btn-soft btn-error btn-xs md:btn-sm" onClick={() => { sell({ item: item, amount: 5 }) }} disabled={isSellPending}>
+                                                    {isSellPending ? <span className="loading loading-spinner loading-sm"></span> : 'Sell'}
+                                                </button>
+                                                <button className="btn btn-soft btn-error btn-xs md:btn-sm" onClick={() => { sell({ item: item, amount: 5 }) }} disabled={isSellPending}>
+                                                    {isSellPending ? <span className="loading loading-spinner loading-sm"></span> : 'Sell x5'}
+                                                </button>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                    }
+                </tbody>
+            </table>
         </PageCard>
     )
 }
