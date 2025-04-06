1b8778b7634c84e4d59e5aa08f3a0a9e22e4b630
Grant Sherman <tnarg122@gmail.com>
2025-04-06 01:13:30 +0000

    only try to refetch characters once

--- src/lib/stateMangers.ts
+++ src/lib/stateMangers.ts
@@ -5,11 +5,7 @@ export const useCharacter = () => useQuery({
     queryKey: ['character'],
     queryFn: getCharacter,
     // retryOnMount: false,
-    retry: (failureCount, error) => {
-        // Don't retry if character is not found the first time
-        if (error.message === 'Not Found') return false;
-        return failureCount < 3;
-    }
+    retry: 1
 });
 
 export const useInventory = () => useQuery({
