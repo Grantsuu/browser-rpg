8b28c5fee1ad17e75e5289e66f75d172e3d172ea
Grant Sherman <tnarg122@gmail.com>
2025-04-07 05:05:34 +0000

    made fishing loading less intrusive

--- src/containers/Fishing.tsx
+++ src/containers/Fishing.tsx
@@ -29,7 +29,7 @@ const Fishing = () => {
 
     const [area, setArea] = useState(data?.area);
 
-    const { mutateAsync: startFishing, isPending: isStarting } = useMutation({
+    const { mutateAsync: startFishing, isPending } = useMutation({
         mutationFn: () => postStartFishingGame(area.name),
         onSuccess: (data) => {
             setDisableTiles(false);
@@ -47,8 +47,6 @@ const Fishing = () => {
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    if (isLoading || isStarting) return <span className="loading loading-spinner loading-xl"></span>;
-
     if (error) {
         return toast.error(`Something went wrong fetching fishing: ${(error as Error).message}`);
     };
@@ -61,98 +59,104 @@ const Fishing = () => {
 
     return (
         <PageCard title="Fishing" icon={faFish}>
-            {display === 'AreaSelection' ?
-                // Area Selection Screen
-                <div>
-                    <FishingAreaSelection setDisplay={setDisplay} setArea={setArea} setDisableTiles={setDisableTiles} startFishing={startFishing} />
-                </div>
-                // Fishing Game Board
-                :
-                <div>
-                    {/* Info */}
-                    <div className="flex flex-col w-full text-center items-center justify-between lg:mb-4">
-                        <div className="flex flex-row items-center gap-2 text-2xl font-bold">
-                            <FontAwesomeIcon icon={faWater as IconProp} />
-                            {data?.area?.name}
+            {(isLoading || display === 'AreaSelection' && isPending) ?
+                <span className="h-full loading loading-spinner loading-xl self-center"></span> :
+                display === 'AreaSelection' ?
+                    // Area Selection Screen
+                    <div>
+                        <FishingAreaSelection setDisplay={setDisplay} setArea={setArea} setDisableTiles={setDisableTiles} startFishing={startFishing} />
+                    </div>
+                    // Fishing Game Board
+                    :
+                    <div>
+                        {/* Info */}
+                        <div className="flex flex-col w-full text-center items-center justify-between lg:mb-4">
+                            <div className="flex flex-row items-center gap-2 text-2xl font-bold">
+                                <FontAwesomeIcon icon={faWater as IconProp} />
+                                {data?.area?.name}
+                            </div>
+                            <div className="flex flex-col mb-2 items-center">
+                                <div className="flex grid grid-cols-3 items-end justify-center gap-4">
+                                    <button className="btn btn-primary" onClick={() => setDisplay('AreaSelection')}><FontAwesomeIcon icon={faArrowLeft as IconProp} /><div className="hidden sm:inline-block">Select Area</div></button>
+                                    <div className="">
+                                        <div className="text-lg">Attempts Left</div>
+                                        <div className={`text-4xl md:text-5xl ${(data?.turns === data?.area.max_turns) ? "text-red-500" : (data?.turns > Math.floor(data?.area.max_turns * 0.5)) ? "text-yellow-500" : "text-blue-500"}`}>
+                                            {data?.area?.max_turns - data.turns}/{data?.area?.max_turns}
+                                        </div>
+                                    </div>
+                                    <button className="btn btn-secondary" onClick={() => { handleReset() }} disabled={isPending}><FontAwesomeIcon icon={faRotateLeft as IconProp} /><div className="hidden sm:inline-block">Reset</div></button>
+                                </div>
+                            </div>
                         </div>
-                        <div className="flex flex-col mb-2 items-center">
-                            <div className="flex grid grid-cols-3 items-end justify-center gap-4">
-                                <button className="btn btn-primary" onClick={() => setDisplay('AreaSelection')}><FontAwesomeIcon icon={faArrowLeft as IconProp} /><div className="hidden sm:inline-block">Select Area</div></button>
-                                <div className="">
-                                    <div className="text-lg">Attempts Left</div>
-                                    <div className={`text-4xl md:text-5xl ${(data?.turns === data?.area.max_turns) ? "text-red-500" : (data?.turns > Math.floor(data?.area.max_turns * 0.5)) ? "text-yellow-500" : "text-blue-500"}`}>
-                                        {data?.area?.max_turns - data.turns}/{data?.area?.max_turns}
+                        <div className="flex flex-col justify-around sm:flex-row gap-1 md:gap-2">
+                            {/* Fishing Board */}
+                            <div className="relative w-full sm:w-1/2 lg:w-1/2 xl:w-1/4">
+                                <div className={clsx(
+                                    "grid",
+                                    "gap-1",
+                                    { "grid-cols-3": data?.area?.size?.cols === 3 },
+                                    { "grid-cols-4": data?.area?.size?.cols === 4 },
+                                    { "grid-cols-5": data?.area?.size?.cols === 5 }
+                                )}>
+                                    {/* Tiles */}
+                                    {data?.game_state?.tiles.map((row: string[], rowIndex: number) => {
+                                        return row.map((label: string, colIndex: number) => {
+                                            return (
+                                                <FishingTile key={`${rowIndex}-${colIndex}`} label={label} row={rowIndex} col={colIndex} disabled={disableTiles} setDisabled={setDisableTiles} />
+                                            )
+                                        })
+                                    })}
+                                </div>
+                                {isPending &&
+                                    <div className={`absolute text-center text-white bg-gray-700/75 bottom-0 left-0 w-full h-full rounded-xl transition-all ease-in-out duration-300 visible opacity-100`}>
+                                        <span className="w-1/6 h-full loading loading-spinner loading-xl self-center"></span>
+                                    </div>}
+                                <div className={`invisible absolute text-center bg-gray-700/75 bottom-0 left-0 w-full h-full rounded-xl transition-all ease-in-out duration-300 ${(data?.turns === data?.area.max_turns && !isPending) ? "visible opacity-100" : "opacity-0"}`}>
+                                    <div className="flex flex-col items-center justify-center h-full gap-2">
+                                        <button className="btn btn-primary btn-lg" onClick={() => setDisplay('AreaSelection')}><FontAwesomeIcon icon={faArrowLeft as IconProp} />Select Area</button>
+                                        <button className="btn btn-secondary btn-lg" onClick={() => { handleReset() }} disabled={isPending}><FontAwesomeIcon icon={faRotateLeft as IconProp} />New Game</button>
                                     </div>
                                 </div>
-                                <button className="btn btn-secondary" onClick={() => { handleReset() }} disabled={isStarting}><FontAwesomeIcon icon={faRotateLeft as IconProp} /><div className="hidden sm:inline-block">Reset</div></button>
                             </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-col justify-around sm:flex-row gap-1 md:gap-2">
-                        {/* Fishing Board */}
-                        <div className="relative w-full sm:w-1/2 lg:w-1/2 xl:w-1/4">
-                            <div className={clsx(
-                                "grid",
-                                "gap-1",
-                                { "grid-cols-3": data?.area?.size?.cols === 3 },
-                                { "grid-cols-4": data?.area?.size?.cols === 4 },
-                                { "grid-cols-5": data?.area?.size?.cols === 5 }
-                            )}>
-                                {/* Tiles */}
-                                {data?.game_state?.tiles.map((row: string[], rowIndex: number) => {
-                                    return row.map((label: string, colIndex: number) => {
-                                        return (
-                                            <FishingTile key={`${rowIndex}-${colIndex}`} label={label} row={rowIndex} col={colIndex} disabled={disableTiles} setDisabled={setDisableTiles} />
-                                        )
-                                    })
-                                })}
-                            </div>
-                            <div className={`invisible absolute bg-gray-700/75 bottom-0 left-0 w-full h-full rounded-xl transition-all ease-in-out duration-300 ${data?.turns === data?.area.max_turns ? "visible opacity-100" : "opacity-0"}`}>
-                                <div className="flex flex-col items-center justify-center h-full gap-2">
-                                    <button className="btn btn-primary btn-lg" onClick={() => setDisplay('AreaSelection')}><FontAwesomeIcon icon={faArrowLeft as IconProp} />Select Area</button>
-                                    <button className="btn btn-secondary btn-lg" onClick={() => { handleReset() }} disabled={isStarting}><FontAwesomeIcon icon={faRotateLeft as IconProp} />New Game</button>
+                            {/* Legend */}
+                            <div className="w-full sm:w-1/2 lg:w-1/3 items-center">
+                                <div className="flex flex-row items-center justify-center gap-2 text-xl font-bold">
+                                    <FontAwesomeIcon icon={faQuestion as IconProp} className="text-sm aspect-square border-2 rounded-full p-1" />
+                                    Legend
                                 </div>
+                                <table className="table md:table-sm lg:table-sm xl:table-md text-center">
+                                    <thead>
+                                        <tr>
+                                            <th>Symbol</th>
+                                            <th>Meaning</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={faQuestion as IconProp} /></td>
+                                            <td>Undiscovered</td>
+                                        </tr>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={faHashtag as IconProp} className="aspect-square border-2 p-1" /></td>
+                                            <td>Adjacent Unempty Tiles</td>
+                                        </tr>
+                                        <tr>
+                                            <td><FontAwesomeIcon icon={faFishFins as IconProp} className="border-2 rounded-full p-1" /></td>
+                                            <td>Fish</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="text-yellow-400"><FontAwesomeIcon icon={faFishFins as IconProp} className="border-2 rounded-full p-1" /></td>
+                                            <td>Bountiful Fish</td>
+                                        </tr>
+                                        <tr>
+                                            <td className="text-yellow-400"><FontAwesomeIcon icon={faExclamation as IconProp} className="aspect-square border-2 rounded-full p-1" /></td>
+                                            <td>Random Event</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
                             </div>
-                        </div>
-                        {/* Legend */}
-                        <div className="w-full sm:w-1/2 lg:w-1/3 items-center">
-                            <div className="flex flex-row items-center justify-center gap-2 text-xl font-bold">
-                                <FontAwesomeIcon icon={faQuestion as IconProp} className="text-sm aspect-square border-2 rounded-full p-1" />
-                                Legend
-                            </div>
-                            <table className="table md:table-sm lg:table-sm xl:table-md text-center">
-                                <thead>
-                                    <tr>
-                                        <th>Symbol</th>
-                                        <th>Meaning</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td><FontAwesomeIcon icon={faQuestion as IconProp} /></td>
-                                        <td>Undiscovered</td>
-                                    </tr>
-                                    <tr>
-                                        <td><FontAwesomeIcon icon={faHashtag as IconProp} className="aspect-square border-2 p-1" /></td>
-                                        <td>Adjacent Unempty Tiles</td>
-                                    </tr>
-                                    <tr>
-                                        <td><FontAwesomeIcon icon={faFishFins as IconProp} className="border-2 rounded-full p-1" /></td>
-                                        <td>Fish</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="text-yellow-400"><FontAwesomeIcon icon={faFishFins as IconProp} className="border-2 rounded-full p-1" /></td>
-                                        <td>Bountiful Fish</td>
-                                    </tr>
-                                    <tr>
-                                        <td className="text-yellow-400"><FontAwesomeIcon icon={faExclamation as IconProp} className="aspect-square border-2 rounded-full p-1" /></td>
-                                        <td>Random Event</td>
-                                    </tr>
-                                </tbody>
-                            </table>
                         </div>
                     </div>
-                </div>
             }
         </PageCard >
     )
