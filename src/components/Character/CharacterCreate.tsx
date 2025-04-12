a591456fbf26863aaaf71d44bbd799b084f1d771
Grant Sherman <tnarg122@gmail.com>
2025-04-12 21:03:35 +0000

    installed zod and use hook form and refactored character create to use these instead of yup/formik

--- src/components/Character/CharacterCreate.tsx
+++ src/components/Character/CharacterCreate.tsx
@@ -1,30 +1,38 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from 'react-toastify';
-import { Form, Formik, useField } from 'formik';
-import * as Yup from 'yup';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from "@hookform/resolvers/zod";
 import { postCreateCharacter } from '../../lib/apiClient';
 import { useNavigate } from 'react-router';
 
-const FormTextInput = ({ ...props }) => {
-    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
-    // which we can spread on <input>. We can use field meta to show an error
-    // message if the field is invalid and it has been touched (i.e. visited)
-    const [field, meta] = useField(props);
-    return (
-        <div className='mb-2'>
-            <input className={props.className} {...field} {...props} />
-            {meta.touched && meta.error ? (
-                <div className="ml-1 text-xs text-red-600">{meta.error}</div>
-            ) : null}
-        </div>
-    );
-};
+const CharacterCreateSchema = z.object({
+    name: z.string()
+        .min(3, { message: 'Must be at least 3 characters' })
+        .max(20, { message: 'Must be 20 characters or less' })
+        .nonempty({ message: 'Required' }),
+});
+
+type CharacterCreateFormValues = z.infer<typeof CharacterCreateSchema>;
+
 const CharacterCreate = () => {
     const queryClient = useQueryClient();
     const navigate = useNavigate();
 
-    const { mutate, isPending } = useMutation({
-        mutationFn: postCreateCharacter,
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting }
+    } = useForm<CharacterCreateFormValues>({
+        resolver: zodResolver(CharacterCreateSchema),
+    });
+
+    const onSubmit: SubmitHandler<CharacterCreateFormValues> = async (data) => {
+        await mutateAsync(data);
+    };
+
+    const { mutateAsync } = useMutation({
+        mutationFn: (variables: { name: string }) => postCreateCharacter(variables.name),
         onSuccess: () => {
             toast.success('Character created successfully!');
             queryClient.invalidateQueries({ queryKey: ['character'] });
@@ -36,34 +44,26 @@ const CharacterCreate = () => {
     })
 
     return (
-        <Formik
-            initialValues={{ name: '' }}
-            validationSchema={Yup.object({
-                name: Yup.string()
-                    .required('Required')
-                    .min(3, 'Must be at least 3 characters')
-                    .max(20, 'Must be 20 characters or less'),
-            })}
-            onSubmit={(values) => {
-                mutate(values.name);
-            }}
-        >
-            <Form>
-                <FormTextInput
-                    className='w-full p-2 border border-gray-300 rounded'
-                    name='name'
-                    type='text'
-                    placeholder='Character Name'
-                />
-                <button
-                    className='btn btn-primary w-full p-2 mt-2 text-white rounded'
-                    type='submit'
-                    disabled={isPending}
-                >
-                    {isPending ? <span className="loading loading-spinner loading-sm"></span> : 'Create Character'}
-                </button>
-            </Form>
-        </Formik>
+        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col w-full'>
+            {/* Name */}
+            <input
+                className='input w-full p-2 border border-gray-300 rounded'
+                type='text'
+                placeholder='Character Name'
+                disabled={isSubmitting}
+                {...register('name')}
+            />
+            {/* Error Output */}
+            {errors.name && <span className="ml-1 text-xs text-error">{errors.name.message}</span>}
+            {/* Submit Button */}
+            <button
+                className='btn btn-primary w-full p-2 mt-2 text-white rounded'
+                type='submit'
+                disabled={isSubmitting}
+            >
+                {isSubmitting ? <span className="loading loading-spinner loading-sm"></span> : 'Create Character'}
+            </button>
+        </form>
     );
 }
 
