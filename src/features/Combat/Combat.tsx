aada8e81f4a064a0817216430e7c0fad2bfd1b97
Grant Sherman <tnarg122@gmail.com>
2025-04-17 08:22:01 +0000

    animation for damage

--- src/features/Combat/Combat.tsx
+++ src/features/Combat/Combat.tsx
@@ -1,4 +1,6 @@
+import { useEffect, useState } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AnimatePresence, motion } from "motion/react"
 import { toast } from 'react-toastify';
 import type { CombatData } from "../../types/types";
 import { useCharacter } from "../../lib/stateMangers";
@@ -12,9 +14,18 @@ const Combat = ({ combat }: CombatProps) => {
     const queryClient = useQueryClient();
     const { data: character } = useCharacter();
 
+    const [showAnimation, setShowAnimation] = useState(false);
+
+    useEffect(() => {
+        if (showAnimation) {
+            setShowAnimation(false);
+        }
+    }, [showAnimation]);
+
     const { mutateAsync: updateCombat, isPending: combatLoading } = useMutation({
         mutationFn: (variables: { action: string, monsterId?: number }) => putUpdateCombat(variables.action, variables.monsterId),
         onSuccess: (data) => {
+            setShowAnimation(true);
             queryClient.setQueryData(['combat'], data);
         },
         onError: (error) => {
@@ -37,7 +48,21 @@ const Combat = ({ combat }: CombatProps) => {
             <div className="card card-lg border border-gray-100 shadow-md w-full lg:w-1/3">
                 <div className="card-body p-3 items-center">
                     <h2 className="card-title text-center">{combat.monster.name}</h2>
-                    <img src={combat.monster.image} alt={combat.monster.name} title={combat.monster.name} className="w-1/3" />
+                    <div className="relative z-0 w-1/3">
+                        <AnimatePresence initial={false}>
+                            {combat?.state?.last_actions?.player?.action === 'attack' && showAnimation && (
+                                <motion.div
+                                    initial={{ opacity: 1, y: 0 }}
+                                    exit={{ opacity: 0.0, y: -75 }}
+                                    transition={{ duration: 1.00, ease: "circOut" }}
+                                    key="monster-damage-animation"
+                                    className="absolute z-10 flex justify-center items-center w-full h-full top-0 left-0"
+                                >
+                                    <div className="text-red-500 text-2xl font-bold">{combat?.state?.last_actions?.player?.amount}</div>
+                                </motion.div>)}
+                        </AnimatePresence >
+                        <img src={combat.monster.image} alt={combat.monster.name} title={combat.monster.name} />
+                    </div>
                     <div className="flex flex-row gap-1">
                         <img src="images/heart.png" className="w-5" />
                         <div className="font-semibold">{combat.monster.health}/{combat.monster.max_health}</div>
@@ -148,7 +173,10 @@ const Combat = ({ combat }: CombatProps) => {
                             </button>
                             <button
                                 className="btn btn-success"
-                                disabled={combatLoading}>
+                                disabled={combatLoading}
+                                onClick={() => {
+                                    setShowAnimation(true);
+                                }}>
                                 {combatLoading ?
                                     <span className="loading loading-spinner loading-sm self-center"></span> :
                                     <><img src="images/backpack.png" className="w-5" />Items</>}
