bf874ce15c746749b9e0e471b90fb2b528f48724
Grant Sherman <tnarg122@gmail.com>
2025-04-20 21:39:13 +0000

    connect stats screen to character levels table

--- src/components/Character/CharacterStats.tsx
+++ src/components/Character/CharacterStats.tsx
@@ -1,24 +1,38 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoins, faFish, faSeedling, faKitchenSet } from "@fortawesome/free-solid-svg-icons";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { useCharacter } from '../../lib/stateMangers';
+import { useCharacter, useCharacterLevels } from '../../lib/stateMangers';
 import { experience_table } from '../../constants/game/experience_table';
 
 const CharacterStats = () => {
-    const { data } = useCharacter();
+    const { data: character } = useCharacter();
+    const { data: levels } = useCharacterLevels();
     return (
         <div className="flex flex-col gap-4">
             <div className="grid grid-cols-2 gap-4">
                 <div>
                     <h3 className="text-lg font-semibold">Name</h3>
-                    <p>{data?.name}</p>
+                    <p>{character?.name}</p>
                 </div>
                 <div>
                     <div className="flex flex-row items-center gap-2">
                         <FontAwesomeIcon icon={faCoins as IconProp} />
                         <h3 className="text-lg font-semibold">Gold</h3>
                     </div>
-                    <p>{data?.gold}</p>
+                    <p>{character?.gold}</p>
+                </div>
+            </div>
+            <div className="mt-4 prose">
+                <h2>Combat</h2>
+            </div>
+            <div className="grid grid-cols-3 gap-4">
+                <div>
+                    <div className="flex flex-row items-center gap-2">
+                        <img src="images/swords.png" className="w-5" />
+                        <h3 className="text-lg font-semibold">Combat</h3>
+                    </div>
+                    <p>Level: {levels?.combat_level}</p>
+                    <p>Experience: {levels?.combat_experience}/{experience_table[levels?.combat_level + 1 as keyof typeof experience_table]}</p>
                 </div>
             </div>
             <div className="mt-4 prose">
@@ -30,24 +44,24 @@ const CharacterStats = () => {
                         <FontAwesomeIcon icon={faSeedling as IconProp} />
                         <h3 className="text-lg font-semibold">Farming</h3>
                     </div>
-                    <p>Level: {data?.farming_level}</p>
-                    <p>Experience: {data?.farming_experience}/{experience_table[data?.farming_level + 1 as keyof typeof experience_table]}</p>
+                    <p>Level: {levels?.farming_level}</p>
+                    <p>Experience: {levels?.farming_experience}/{experience_table[levels?.farming_level + 1 as keyof typeof experience_table]}</p>
                 </div>
                 <div>
                     <div className="flex flex-row items-center gap-2">
                         <FontAwesomeIcon icon={faKitchenSet as IconProp} />
                         <h3 className="text-lg font-semibold">Cooking</h3>
                     </div>
-                    <p>Level: {data?.cooking_level}</p>
-                    <p>Experience: {data?.cooking_experience}/{experience_table[data?.cooking_level + 1 as keyof typeof experience_table]}</p>
+                    <p>Level: {levels?.cooking_level}</p>
+                    <p>Experience: {levels?.cooking_experience}/{experience_table[levels?.cooking_level + 1 as keyof typeof experience_table]}</p>
                 </div>
                 <div>
                     <div className="flex flex-row items-center gap-2">
                         <FontAwesomeIcon icon={faFish as IconProp} />
                         <h3 className="text-lg font-semibold">Fishing</h3>
                     </div>
-                    <p>Level: {data?.fishing_level}</p>
-                    <p>Experience: {data?.fishing_experience}/{experience_table[data?.fishing_level + 1 as keyof typeof experience_table]}</p>
+                    <p>Level: {levels?.fishing_level}</p>
+                    <p>Experience: {levels?.fishing_experience}/{experience_table[levels?.fishing_level + 1 as keyof typeof experience_table]}</p>
                 </div>
             </div>
         </div>
